e9cfe861af39e35c1af3feb7972f90b8f2e25d3f
Aforizmadam44 <huseynovismayil5@gmail.com>
2023-12-30 13:59:17 +0400

    header responsive finished

--- src/components/Grow/index.jsx
+++ src/components/Grow/index.jsx
@@ -1,5 +1,4 @@
 import Button from "../Button";
-import Dashboard from "../Dashboard";
 
 const Grow = () => {
   return (
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,18 +2,18 @@ import Button from "../Button";
 
 const Header = () => {
   return (
-    <div className="container my-20 flex justify-center  text-center flex-col gap-6 text-primary font-bold w-[70%]  ">
-      <h1 className="text-5xl mb-6">
+    <div className="container my-20 flex  text-center items-center flex-col gap-6 text-primary font-bold w-[60%] ">
+      <h1 className="text-5xl font-semibold">
         Satışlarınızı və xidmətlərinizi inkişaf etdirmək üçün ən yaxşı proqram
       </h1>
-      <p className="font-semibold">
+      <p className="font-semibold text-slate-500">
         Bu proqram vasitəsiylə uzun illərin təcrübəsinə əsaslanaraq sizə unikal
         təkliflər və professional xidmətlər təqdim edirik, bizə qoşulun və
         perspektiv təminata sahiblənin.
       </p>
       <div className="inputAndBtn">
         <input
-          type="text"
+          type="email"
           placeholder="Enter your mail"
           className="border-slate-300 rounded-2xl my-6 w-96 h-16"
         />
